1e772a49270d52d71843c14cc5f0d180d5e8b715
rabianadeem-121 <rn.aibar.rn@gmail.com>
2025-12-15 18:09:03 +0500

    added secrets-edited

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "api_server" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t3.micro"
-  subnet_id     = aws_subnet.public.id
+  subnet_id     = aws_subnet.public_a.id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   iam_instance_profile  = aws_iam_instance_profile.ec2_profile.name
   key_name = var.key_name
