b2e76db994d09ba0088792a713960f1069c52b49
rabianadeem-121 <rn.aibar.rn@gmail.com>
2025-12-15 18:35:56 +0500

    added secrets-edited

--- terraform/ec2-iam.tf
+++ terraform/ec2-iam.tf
@@ -29,6 +29,6 @@ resource "aws_iam_role_policy_attachment" "cloudwatch_attach" {
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
 resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "ec2-instance-profile"
+   name = "ec2-instance-profile-${random_id.suffix.hex}"
   role = aws_iam_role.ec2_role.name
 }
