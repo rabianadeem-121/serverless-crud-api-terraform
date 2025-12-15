2f02b9b2b5c54e48f98f53058fd3892fd6041f2d
rabianadeem-121 <rn.aibar.rn@gmail.com>
2025-12-15 12:25:02 +0500

    Setup ECR Terraform, update GitHub Actions for OIDC

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -2,17 +2,6 @@ resource "aws_db_subnet_group" "main" {
   subnet_ids = aws_subnet.private[*].id
 }
 
-resource "aws_security_group" "rds_sg1" {
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-}
-
 resource "aws_db_instance" "postgres" {
   engine            = "postgres"
   instance_class    = "db.t3.micro"
