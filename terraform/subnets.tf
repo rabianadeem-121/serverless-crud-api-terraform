b6cbbf7fe1392fcd16222ea99eee2df144394d1a
rabianadeem-121 <rn.aibar.rn@gmail.com>
2025-12-15 17:52:26 +0500

    added secrets

---
+++ terraform/subnets.tf
@@ -0,0 +1,21 @@
+# Public subnet
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# Private subnet
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1a"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "private-subnet"
+  }
+}
