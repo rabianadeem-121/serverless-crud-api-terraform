0cd0f2f8dabc7587b2c542ba903cf6106af90852
rabianadeem-121 <rn.aibar.rn@gmail.com>
2025-12-15 17:58:09 +0500

    added secrets-edited

--- terraform/ec2-iam.tf
+++ terraform/ec2-iam.tf
@@ -10,7 +10,21 @@ resource "aws_iam_role" "ec2_role" {
     }]
   })
 }
+
 resource "aws_iam_role_policy_attachment" "ecr" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+resource "aws_iam_role_policy_attachment" "ec2_attach" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_attach" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2-instance-profile"
+  role = aws_iam_role.ec2_role.name
 }
